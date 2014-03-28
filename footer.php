e19604732c441ebf7317f4edbf80f6453b6abad0
citylike <citylikeproject@gmail.com>
2014-03-28 20:11:28 +0400

    minor changes vk auth

--- footer.php
+++ footer.php
@@ -1,4 +1,17 @@
 	<script type='text/javascript' src='assets/scripts/main.js'></script>
 	<script type='text/javascript' src='assets/scripts/bootstrap.min.js'></script>
+	<script language="javascript">
+		VK.init({
+		  apiId: 4206762
+		});
+		function authInfo(response) {
+		  if (response.session) {
+			alert('user: '+response.session.mid);
+		  } else {
+			alert('not auth');
+		  }
+		}
+		VK.Auth.getLoginStatus(authInfo);
+	</script>
 </body>
 </html>
